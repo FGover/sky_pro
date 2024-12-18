77cf644f872cff8bf7ab061bc9690acf7db702b8
fghwzw <2924074463@qq.com>
2024-12-18 14:42:10 +0800

    Initial commit

---
+++ sky_take_out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -0,0 +1,17 @@
+package com.sky;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@EnableTransactionManagement  // 注解事务管理
+@Slf4j
+public class SkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+        log.info("server started");
+    }
+}
